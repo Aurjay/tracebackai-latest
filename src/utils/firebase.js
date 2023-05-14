f40c004aadaeaf6c9b2a77bb31ba4a7bebea7217
Deepak <ddps628@gmail.com>
2023-05-14 15:59:18 +0200

    firebase-new
    fff

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSWPoEqKujrHVMixuEoSnBQQtnmH56NK0",
-  authDomain: "trail-ml-firebase.firebaseapp.com",
-  projectId: "trail-ml-firebase",
-  storageBucket: "trail-ml-firebase.appspot.com",
-  messagingSenderId: "979900479995",
-  appId: "1:979900479995:web:b30268582f587ae467b35e",
-  measurementId: "G-W0VQSTXDSJ"
+  apiKey: "AIzaSyA5edM6PoqUzZm-eK1savgCLAnKa240dpU",
+
+  authDomain: "traceback-ai-43af3.firebaseapp.com",
+
+  projectId: "traceback-ai-43af3",
+
+  storageBucket: "traceback-ai-43af3.appspot.com",
+
+  messagingSenderId: "75382777607",
+
+  appId: "1:75382777607:web:6379bf23dc70fc284847a2",
+
+  measurementId: "G-F4H9F75GXN"
+
 };
 
 // Initialize Firebase
