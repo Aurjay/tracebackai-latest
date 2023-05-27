28742d6a3b9a4d36379c01aecadb285bd2dafde4
Deepak <ddps628@gmail.com>
2023-05-27 14:24:52 +0200

    jgj
    kjh

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   reactStrictMode: false,
   env: {
     API_URL:'https://tracebackaigptv4-4t2r2e6xka-uc.a.run.app/api/new-gpt',
+    RECC_API_URL:'https://tracebackaigpt-reccomendationsv3-4t2r2e6xka-uc.a.run.app/api/new-gpt',
   },
   webpack: config => {
     config.resolve.alias = {
--- src/components/reccomendations_frontend/index.js
+++ src/components/reccomendations_frontend/index.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Card, CircularProgress } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Card, CircularProgress, Button } from '@material-ui/core';
 
-function RecommendationCard({ recommendation, index }) {
+function RecommendationCard({ recommendation }) {
   return (
-    <div className="recommendation-item">
-      <p>{`${index + 1}. ${recommendation}`}</p>
-    </div>
+    <Card className="recommendation-card">
+      {recommendation.split('\n').map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
+    </Card>
   );
 }
 
@@ -13,43 +15,52 @@ function Recommendations() {
   const [recommendations, setRecommendations] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    async function fetchRecommendations() {
-      try {
-        setLoading(true);
-        const response = await fetch('API_ENDPOINT_URL');
-        if (response.ok) {
-          const data = await response.text();
-          setRecommendations(data.split('%'));
-        } else {
-          console.error('Failed to fetch recommendations');
-        }
-      } catch (error) {
-        console.error('Error fetching recommendations:', error);
-      } finally {
-        setLoading(false);
-      }
-    }
+  const fetchRecommendations = async () => {
+    try {
+      setLoading(true);
 
+      const response = await fetch(process.env.RECC_API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setRecommendations(data.answer.split('\n'));
+      } else {
+        console.error('Failed to fetch recommendations');
+      }
+    } catch (error) {
+      console.error('Error fetching recommendations:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleRefresh = () => {
     fetchRecommendations();
-  }, []);
+  };
 
   return (
     <div className="recommendations-container">
+      <Button variant="contained" color="primary" onClick={handleRefresh}>
+        Refresh
+      </Button>
       {loading ? (
         <CircularProgress className="loading-icon" />
       ) : (
-        <Card className="recommendations">
+        <div className="recommendations">
           {recommendations.length > 0 ? (
-            <div className="recommendations-scroll">
-              {recommendations.map((recommendation, index) => (
-                <RecommendationCard key={index} recommendation={recommendation} index={index} />
-              ))}
-            </div>
+            recommendations.map((recommendation, index) => (
+              <RecommendationCard key={index} recommendation={recommendation} />
+            ))
           ) : (
             <p>No recommendations for now. Update the F.I.R document.</p>
           )}
-        </Card>
+        </div>
       )}
     </div>
   );
